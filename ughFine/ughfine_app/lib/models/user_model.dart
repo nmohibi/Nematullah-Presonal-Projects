b43093853bb6827d27a6290fb159206393037728
nmohibi <157065698+nmohibi@users.noreply.github.com>
2026-04-12 19:46:59 -0600

    all fields added to userModel

--- ughFine/ughfine_app/lib/models/user_model.dart
+++ ughFine/ughfine_app/lib/models/user_model.dart
@@ -1,11 +1,27 @@
 class UserModel {
   final String uid;
   final String email;
+  final String name;
+  final int age;
+  final double weightKg;
+  final double heightCm;
+  final String goal;
+  final List<String> gymDays;
+  final String dietPreference;
+  final String healthNotes;
   final bool hasCompletedOnboarding;
 
   const UserModel({
     required this.uid,
     required this.email,
+    required this.name,
+    required this.age,
+    required this.weightKg,
+    required this.heightCm,
+    required this.goal,
+    required this.gymDays,
+    required this.dietPreference,
+    required this.healthNotes,
     this.hasCompletedOnboarding = false,
   });
 }
